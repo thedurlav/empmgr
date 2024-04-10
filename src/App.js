94cb4a2b0ed5e443cf4e242d1deb0859bacf4854
thedurlav <thedurlav@gmail.com>
2024-04-10 11:37:11 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import AdminLogin from './components/AdminLogin';
 import {BrowserRouter,Routes,Router, Route} from 'react-router-dom'
 import CreateEmp from './components/CreateEmp';
+import Home from './components/Home';
 
 function App() {
   return (
     <BrowserRouter>
     <div className="App">
       <Routes>
-        <Route path='/' element={<AdminLogin/>}></Route>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/loginadmin' element={<AdminLogin/>}></Route>
         <Route path='/createEmp' element={<CreateEmp/>}></Route>
       </Routes>
       
---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import NavBar from "./NavBar";
+
+const Home = () => {
+    return (
+        <div className="outer-container bg-info" >
+            <NavBar/>
+            <div className="inner-container mx-auto my-3 bg-light">
+                
+
+            </div>
+        </div>
+     );
+}
+ 
+export default Home;
